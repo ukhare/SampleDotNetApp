5f787999c4a74eb3bbdf63ab2ea4d5ebf711dd31
ukhare <ukhare@outlook.com>
2024-12-09 15:38:04 +0530

    Initial commit .net project

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build-env
+WORKDIR /app
+COPY . ./
+RUN dotnet restore
+RUN dotnet build --no-restore
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+using System;
+namespace SampleDotNetApp
+{
+ class Program 
+ { static void Main(string[] args)
+ {
+  Console.WriteLine("Hello, World!");
+ // Warning: Variable declared but never used 
+  int unusedVariable = 10; // Error: Uncomment the following line to trigger an error
+  Console.WriteLine(undefinedVariable);
+  }
+  }
+  }
